a40ea339767a14527998f5eedc7b2c904ff4e4d7
Pat Dodwell <patrick.dodwell27@gmail.com>
2026-05-01 10:50:01 +1000

    Update login.html path to root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                login: resolve(__dirname, 'public/login.html')
+                login: resolve(__dirname, 'login.html')
             }
         }
     }
