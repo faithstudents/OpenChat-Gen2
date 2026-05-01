878fd3f84fd4e6bc6830d672e5112e6df90bce7d
Pat Dodwell <patrick.dodwell27@gmail.com>
2026-05-01 10:46:10 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/OpenChat-Gen2/',  // replace with your actual repo name
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'public/login.html')
+            }
+        }
+    }
 });
